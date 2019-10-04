276af7a655a75624ab15f01ac74c30f582594f32
wingcommander <garvanjones@gmail.com>
2019-10-04 11:21:31 +0100

    Revert "no message"
    This reverts commit ff85c45094f454694c51843948acf77e2182a6b2.

--- ConvertToLocal.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-	$('[data-date]').each(function () {
-		// the date construct will automatically convert to local time
-		var localDate = new Date(parseInt($(this).attr('data-date')));
-		$(this).html(localDate.toLocaleDateString() + " " + localDate.toLocaleTimeString());
-	});
-});
